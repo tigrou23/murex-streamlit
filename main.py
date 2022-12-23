f5618cea67df46d43739c333e8094c4a524c05c2
abdoutlem <abderrahmane.rhmari@gmail.com>
2022-12-23 18:39:24 +0100

    ajout des filtres

--- main.py
+++ main.py
@@ -11,17 +11,30 @@ st.header('Bookings :')
 
 rep = requests.get('http://abdoutlem1997.pythonanywhere.com/bookings/')
 jsonResp = rep.text
+
+
 df = pd.read_json(jsonResp)
 df.drop('id',inplace=True,axis=1)
-df.drop('etage',inplace=True,axis=1)
 
-cm = 1/2.54
 
-data = df.groupby('roomid').value_counts()
 
-st.dataframe(data)
 
-st.bar_chart(data)
+etage = st.selectbox('Etage',(1,2,3,4,5,6,'All'))
+
+
+if(etage!='All'):
+    df_filtered = df[df['etage']== etage]
+else: 
+    df_filtered = df
+
+
+
+st.bar_chart(df_filtered['roomid'].value_counts())
+
+
+
+
+
 
 
 
