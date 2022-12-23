1c1de2f43bf3a4aa730678af1b08de6388a656a2
abdoutlem <abderrahmane.rhmari@gmail.com>
2022-12-23 16:21:56 +0100

    ouho

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import streamlit as st 
 import requests
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
 
 st.set_page_config(page_title="Murex analytics", layout="wide")
 
@@ -10,10 +13,14 @@ rep = requests.get('http://abdoutlem1997.pythonanywhere.com/bookings/')
 jsonResp = rep.text
 df = pd.read_json(jsonResp)
 df.drop('id',inplace=True,axis=1)
-df.groupby('roomid').value_counts().plot.bar()
 
 
-st.dataframe(df)
+cm = 1/2.54
+
+data = df.groupby('roomid').value_counts()
+
+
+st.bar_chart(data)
 
 
 
