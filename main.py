60ed572116e5a6b0ac6ffdb5d07982f1b18c9d38
abdoutlem <abderrahmane.rhmari@gmail.com>
2022-12-23 15:19:01 +0100

    ajout graph

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ st.header('Bookings :')
 rep = requests.get('http://abdoutlem1997.pythonanywhere.com/bookings/')
 jsonResp = rep.text
 df = pd.read_json(jsonResp)
+df.drop('id',inplace=True,axis=1)
+df.groupby('roomid').value_counts().plot.bar()
 
 
 st.dataframe(df)
