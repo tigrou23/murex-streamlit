f15ca985352f5f7eb5266298121a27952a0bef78
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2023-01-09 16:12:34 +0100

    useless library

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import requests
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import plotly.express as px
 import arrow
 import json
 from datetime import datetime
