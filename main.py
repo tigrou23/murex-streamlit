7be846f5cda7746da3d354b1a5dcb5621acf5f89
Hugo Pereira <54220880+tigrou23@users.noreply.github.com>
2023-02-13 10:06:00 +0100

    inactivity --> test

--- main.py
+++ main.py
@@ -16,67 +16,68 @@ def NumToMonths(shortMonth):
     months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
     return months[int(shortMonth) - 1]
 
-with tabs[1]:
-    try :
-        rep = requests.get('https://hpereira.pythonanywhere.com/bookings/')
-        rep.raise_for_status()
-    except requests.exceptions.HTTPError:
-        st.header("⚠️ Impossible de se connecter à l'API. ⚠️")
-        st.write("Vérifiez votre connexion ou contactez le support.")
-    else :     
-        st.title('Réservations :')     
-        data = json.loads(rep.text)
-        df = pd.DataFrame(data)
-        df.drop('id',inplace=True,axis=1)
 
-        st.write('### Nombre de réservations par salle :')
-        etage = st.selectbox("Sélectionnez l'étage de la salle : ",(1,2,3,4,5,6,7,8,9,'All'))
-        if(etage!='All'):
-            df_filtered = df[df['etage'] == etage]
-        else: 
-            df_filtered = df
-        if(len(df_filtered.index) != 0):
-            st.markdown('- axe x : Id de la salle')
-            st.markdown('- axe y : Nombre de réservations')
-            st.bar_chart(df_filtered['roomid'].value_counts())
-        else:
-            st.warning("Absence de données pour former l'histogramme.")
+# with tabs[1]:
+#     try :
+#         rep = requests.get('https://hpereira.pythonanywhere.com/bookings/')
+#         rep.raise_for_status()
+#     except requests.exceptions.HTTPError:
+#         st.header("⚠️ Impossible de se connecter à l'API. ⚠️")
+#         st.write("Vérifiez votre connexion ou contactez le support.")
+#     else :     
+#         st.title('Réservations :')     
+#         data = json.loads(rep.text)
+#         df = pd.DataFrame(data)
+#         df.drop('id',inplace=True,axis=1)
 
-        st.write('### Nombre de réservations par jour :')
-        df_filtered1 = df["date"].astype(str).str.split("|", n = 1, expand = True)
-        if(len(df_filtered1.index) != 0):
-            df_filtered1.columns = ["jours", "heures"]
-            st.markdown('- axe x : Date')
-            st.markdown('- axe y : Nombre de réservations')
-            st.bar_chart(df_filtered1["jours"].value_counts())
-        else:
-            st.warning("Absence de données pour former l'histogramme.")
+#         st.write('### Nombre de réservations par salle :')
+#         etage = st.selectbox("Sélectionnez l'étage de la salle : ",(1,2,3,4,5,6,7,8,9,'All'))
+#         if(etage!='All'):
+#             df_filtered = df[df['etage'] == etage]
+#         else: 
+#             df_filtered = df
+#         if(len(df_filtered.index) != 0):
+#             st.markdown('- axe x : Id de la salle')
+#             st.markdown('- axe y : Nombre de réservations')
+#             st.bar_chart(df_filtered['roomid'].value_counts())
+#         else:
+#             st.warning("Absence de données pour former l'histogramme.")
 
-        st.write('### Nombre de réservations par heure :')
-        df_filtered2 = df["date"].astype(str).str.split("|", n = 1, expand = True)
-        df_filtered2 = df_filtered2[1].astype(str).str.split(":", n = 1, expand = True)
-        if(len(df_filtered2.index) != 0):
-            df_filtered2.columns = ['heures', 'minutes et secondes']
-            st.markdown('- axe x : Heure')
-            st.markdown('- axe y : Nombre de réservations')
-            st.bar_chart(df_filtered2['heures'].value_counts())
-        else:
-            st.warning("Absence de données pour former l'histogramme.")
+#         st.write('### Nombre de réservations par jour :')
+#         df_filtered1 = df["date"].astype(str).str.split("|", n = 1, expand = True)
+#         if(len(df_filtered1.index) != 0):
+#             df_filtered1.columns = ["jours", "heures"]
+#             st.markdown('- axe x : Date')
+#             st.markdown('- axe y : Nombre de réservations')
+#             st.bar_chart(df_filtered1["jours"].value_counts())
+#         else:
+#             st.warning("Absence de données pour former l'histogramme.")
 
-        st.write('### Données supplémentaires :')
-        moyenne_jours = int(df_filtered1.groupby('jours').size().mean())
-        moyenne_heures = int(df_filtered2['heures'].mode()[0])
-        col4, col5 = st.columns(2)
-        col6, col7 = st.columns(2)
-        col4.metric("Nombre total de réservations", len(df['roomid']))
-        df_filtered1 = df_filtered1[df_filtered1['jours'] == arrow.now().format('DD-MM-YYYY')]
-        col5.metric("Nombre de réservations aujourd'hui", len(df_filtered1))
-        col6.metric("Nombre moyen de réservations par jour", moyenne_jours)
-        col7.metric("Horaire pendant lequel il y a eu le plus de réservations", str(moyenne_heures) + " Heures")
+#         st.write('### Nombre de réservations par heure :')
+#         df_filtered2 = df["date"].astype(str).str.split("|", n = 1, expand = True)
+#         df_filtered2 = df_filtered2[1].astype(str).str.split(":", n = 1, expand = True)
+#         if(len(df_filtered2.index) != 0):
+#             df_filtered2.columns = ['heures', 'minutes et secondes']
+#             st.markdown('- axe x : Heure')
+#             st.markdown('- axe y : Nombre de réservations')
+#             st.bar_chart(df_filtered2['heures'].value_counts())
+#         else:
+#             st.warning("Absence de données pour former l'histogramme.")
+
+#         st.write('### Données supplémentaires :')
+#         moyenne_jours = int(df_filtered1.groupby('jours').size().mean())
+#         moyenne_heures = int(df_filtered2['heures'].mode()[0])
+#         col4, col5 = st.columns(2)
+#         col6, col7 = st.columns(2)
+#         col4.metric("Nombre total de réservations", len(df['roomid']))
+#         df_filtered1 = df_filtered1[df_filtered1['jours'] == arrow.now().format('DD-MM-YYYY')]
+#         col5.metric("Nombre de réservations aujourd'hui", len(df_filtered1))
+#         col6.metric("Nombre moyen de réservations par jour", moyenne_jours)
+#         col7.metric("Horaire pendant lequel il y a eu le plus de réservations", str(moyenne_heures) + " Heures")
 
 with tabs[0]:
     try :
-        rep = requests.get('https://hpereira.pythonanywhere.com/inactivity/')
+        rep = requests.get('https://hpereira.pythonanywhere.com/test/')
         rep.raise_for_status()
     except requests.exceptions.HTTPError:
         st.header("⚠️ Impossible de se connecter à l'API. ⚠️")
